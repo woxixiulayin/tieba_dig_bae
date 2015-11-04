8943bc74dfc51e80ff536d5f33fd5f29bd79b718
woxixiulayin <zhigang7536308@163.com>
2015-11-04 22:49:16 +0800

    将server.py改成默认的入口文件index.py，

--- index.py
+++ index.py
@@ -1,11 +1,35 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python
+#!conding:utf-8
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, send, emit
+from rat import Query
+# import json
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
-    return body
+app = Flask(__name__)
+socketio = SocketIO(app)
 
-from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+event_name = 'tieba_dig'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on(event_name)
+def handle_search_para(para):
+    print para
+    print type(para['rep_num'])
+    posts = []
+    try:
+        query = Query(para)
+        print 'done'
+        posts = query.find()
+    except StandardError, e:
+        print e
+    emit(event_name, posts)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8888)
+else:
+    from bae.core.wsgi import WSGIApplication
+    application = WSGIApplication(socketio)
