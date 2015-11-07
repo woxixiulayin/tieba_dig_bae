247a48c1d1fe76c98f20cba9fda53f15607eef74
woxixiulayin <zhigang7536308@163.com>
2015-11-07 23:59:06 +0800

    add flask and react

--- index.py
+++ index.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
-    return body
+from flask import Flask, render_template, Response, request
 
-from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        print request.form.to_dict()
+
+	return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8888)
+else:
+	from bae.core.wsgi import WSGIApplication
+	application = WSGIApplication(app)
