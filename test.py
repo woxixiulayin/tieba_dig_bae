9c19a91e13650f1a50af4f72ffd46ae7c19ccce6
Jackson Liu <416883813@qq.com>
2016-03-19 11:37:51 +0800

    add some change in test.py

--- test.py
+++ test.py
@@ -15,13 +15,13 @@ class Testcase(unittest.TestCase):
     def test_query_default(self):
         condition = {
             'tieba_name': "戒色",
-            'deepth': 5,
+            'deepth': 10,
             'rep_num': 1000,
             'author': ''
         }
         # self.assertEquals(type(Query(**condition).find()), list)
-        logging.info('get_posts_single_theard find number is: %d' % len(Query(condition).get_posts_single_theard()))
-        # logging.info('get_posts_gevent find number is: %d' % len(Query(condition).get_posts_gevent()))
+        # logging.info('get_posts_single_theard find number is: %d' % len(Query(condition).get_posts_single_theard()))
+        logging.info('get_posts_gevent find number is: %d' % len(Query(condition).get_posts_gevent()))
         # logging.info('get_posts_mutil_theard find number is: %d' % len(Query(condition).get_posts_mutil_theard()))
         # self.assertTrue(len(Query(**condition).find())>0, True)
 
