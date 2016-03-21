c0267323b15969e953ba4b721b8450af9f95036a
Jackson Liu <416883813@qq.com>
2016-03-21 21:33:02 +0800

    use json to define animation descrition. use postIn func to define animation action(for different operation later). use jquery operater animation first,js native will be uesd for learning in the furture

--- static/build/post.js
+++ static/build/post.js
@@ -1,7 +1,22 @@
-var main = function(){
-    $(".post").css({"left":"800px","opacity":"0"})
-    .animate({left: "0", opacity: "1"}, 1200);
+var jPostAnimation = {
+    before: {
+        left: "800px",
+        opacity: "0"
+    },
+    after: {
+        left: "0",
+        opacity: "1"
+    },
+    delay: 1200
+}
 
+function postIn(post, animation){
+    post.css(animation.before)
+    .animate(animation.after, animation.delay);
+}
+
+var main = function(){
+    postIn($(".post"), jPostAnimation);
 };
 
 $(document).ready(main);
