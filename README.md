02d035f01ebc4ba3bda301b21aca1cb8d1736c76
Jackson Liu <416883813@qq.com>
2016-06-21 10:58:56 +0800

    init bae

---
+++ README.md
@@ -0,0 +1,12 @@
+####实例网站
+http://tiebadig.duapp.com/
+####用途
+>按照指定条件搜索百度贴吧中的帖子，部署在百度应用引擎BAE上
+
+####Start
+>安装依赖环境：sudo pip install -r requirements.txt<br>
+>在本地开启服务器： /index.py<br>
+>在浏览器中输入localhost:8888
+
+####技术
+>python（爬虫），flask（网站服务器），js，react（页面展示），AJAX
--- index.py
+++ index.py
@@ -1,31 +1,36 @@
+#!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body_str = """
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Welcome to BAE</title>
-        </head>
-        <body>
-            <h4>Welcome to Baidu App Engine!</h4>
-            <h2>欢迎开启BAE之旅！</h2>
-            <br>
-            <br>
-            <ul>以下链接可能对您有用：</ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/QuickGuide.html" target="_blank">入门指南</a></ul>
-            <ul><a href="http://developer.baidu.com/forum/topic/list?boardId=66" target="_blank">论坛</a></ul>
-            <ul><a href="http://www.baeblog.com/" target="_blank">技术博客</a></ul>
-            <ul><a href="http://weibo.com/baiduappengine" target="_blank">微博</a></ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/FAQ.html" target="_blank">常见问题</a></ul>
-        </body>
-    </html>
-    """
-    body=[body_str]
-    return body
+from flask import Flask, render_template, Response, request
+from rat import Query
+import json
 
-from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+    	para =  request.form.to_dict()
+    	para['deepth'] = int(para['deepth'])
+    	para['rep_num'] = int(para['rep_num'])
+        posts = []
+    try:
+        query = Query(para)
+        print 'done'
+        posts = query.find()
+        return Response(json.dumps(posts), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+    except StandardError, e:
+        print e
+        import traceback
+        traceback.print_exc()
+        return Response(json.dumps(posts), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8888)
+else:
+	from bae.core.wsgi import WSGIApplication
+	application = WSGIApplication(app)
